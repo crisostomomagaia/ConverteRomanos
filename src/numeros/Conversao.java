45cb7ca872bf8b0a91a5c29720300edd9d278524
Inalda <inaldamarina@gmail.com>
2016-08-25 16:23:35 +0200

    conversao de dezenas completo[C

--- src/numeros/Conversao.java
+++ src/numeros/Conversao.java
@@ -3,43 +3,113 @@ package numeros;
 public class Conversao {
 	private String romanos;
 	
-	public String converteUnidades(int numero){
-		romanos="";
-		String unidade="I";
+	public String converte(int numero){
 		
 		if(validaNumero(numero)){
-			if(numero<=5){
-				if(numero<=3){
-					for(int i=0;i<numero;i++){
-						romanos=romanos.concat("I");
-					}
-				}
-				else{
-					romanos="V";
-					for(int i=0;i<5-numero;i++){
-						romanos=unidade.concat(romanos);
-					}
-				}
+			
+			if(numero>0 && numero<=10){
+				
+				romanos=unidade(numero);
 			}
-			else if(numero<=10){
-				romanos="V";
-				if(numero<=8){
-					for(int i=0;i<numero-5;i++){
-						romanos=romanos.concat("I");
-					}
-				}
-				else{
-					romanos="X";
-					for(int i=0;i<10-numero;i++){
-						romanos=unidade.concat(romanos);
-					}
-				}
+			
+			else if(numero>10&&numero<=100){
+				//System.out.println(numero);
+				romanos=dezenas(numero);
 			}
-		}else{
+			
+			else if(numero>100||numero<=1000){
+				
+			}
+		}
+		
+		else{
 			throw new RuntimeException("Numero Invalido");
 		}
 		
 		return romanos;
+	}
+
+	private String unidade(int numero) {
+		romanos="";
+		String unidade="I";
+		if(numero<=5){
+			if(numero<=3){
+				for(int i=0;i<numero;i++){
+					romanos=romanos.concat(unidade);
+				}
+			}
+			else{
+				romanos="V";
+				for(int i=0;i<5-numero;i++){
+					romanos=unidade.concat(romanos);
+				}
+			}
+		}
+		else if(numero<=10){
+			romanos="V";
+			if(numero<=8){
+				for(int i=0;i<numero-5;i++){
+					romanos=romanos.concat("I");
+				}
+			}
+			else{
+				romanos="X";
+				for(int i=0;i<10-numero;i++){
+					romanos=unidade.concat(romanos);
+				}
+			}
+		}
+		return romanos;
+	}
+	
+	
+	//falta acrescentar para 100
+	private String dezenas(int numero){
+		romanos="";
+		String unidade="X";
+		String dezena;
+		if(String.valueOf(numero).length()==2){
+			dezena=String.valueOf((String.valueOf(numero)).charAt(0));
+		}
+		else{
+			dezena=String.valueOf((String.valueOf(numero)).substring(0, 2));
+		}
+
+		String unid=String.valueOf((String.valueOf(numero)).charAt(1));
+		numero=Integer.parseInt(dezena);
+		
+		if(numero<=5){
+			if(numero<=3){
+				for(int i=0;i<numero;i++){
+					romanos=romanos.concat(unidade);
+				}
+			}
+			else{
+				romanos="L";
+				for(int i=0;i<5-numero;i++){
+					romanos=unidade.concat(romanos);
+				}
+			}
+		}
+		else if(numero<=10){
+			romanos="L";
+			if(numero<=8){
+				for(int i=0;i<numero-5;i++){
+					romanos=romanos.concat(unidade);
+				}
+			}
+			else{
+				romanos="C";
+				for(int i=0;i<10-numero;i++){
+					romanos=unidade.concat(romanos);
+				}
+			}
+		}
+		
+		if(Integer.parseInt(unid)==0){
+			return romanos;
+		}
+		return romanos.concat(unidade(Integer.parseInt(unid)));
 	}
 	
 	private boolean validaNumero(int numero){
--- test/numeros/ConversaoTest.java
+++ test/numeros/ConversaoTest.java
@@ -8,33 +8,64 @@ public class ConversaoTest {
 	@Test
 	public void converteNumeroInferiorOuIgualA3() {
 		Conversao c=new Conversao();
-		assertEquals("III", c.converteUnidades(3));
+		assertEquals("III", c.converte(3));
 		
 	}
 	
 	@Test
 	public void converteSuperiorQue3InferiorOuIgual5(){
 		Conversao c=new Conversao();
-		assertEquals("V", c.converteUnidades(5));
+		assertEquals("V", c.converte(5));
 	}
 	
 	@Test
 	public void converteSuperiorQue5eMenorOuIgual8(){
 		Conversao c=new Conversao();
-		assertEquals("VIII", c.converteUnidades(8));
+		assertEquals("VIII", c.converte(8));
 		
 	}
 	
 	@Test
 	public void converteSuperiorQue8Inferior10(){
 		Conversao c=new Conversao();
-		assertEquals("X", c.converteUnidades(10));
+		assertEquals("X", c.converte(10));
 	}
 	
 	@Test(expected=RuntimeException.class)
 	public void naoDeveAceitarNumerosNegativos(){
 		Conversao c=new Conversao();
-		c.converteUnidades(-1);
+		c.converte(-1);
 	}
 
+	@Test
+	public void converteSuperiorQue10EMultiploDe10(){
+		Conversao c=new Conversao();
+		assertEquals("XX", c.converte(20));
+	}
+	
+	@Test
+	public void converteSuperiorQue10Inferior30(){
+		Conversao c=new Conversao();
+		assertEquals("XXIV", c.converte(24));
+	}
+	
+	@Test
+	public void converteSuperiorQue30InferiorQue50(){
+		Conversao c=new Conversao();
+		assertEquals("XLIX", c.converte(49));
+	}
+	
+	@Test
+	public void converteSuperiorQue50Inferior80(){
+		Conversao c=new Conversao();
+		assertEquals("LXXVII", c.converte(77));
+	}
+	
+	@Test
+	public void converteSuperiorA80InferiorOuIgualA100(){
+		Conversao c=new Conversao();
+		assertEquals("XCVI", c.converte(96));
+		assertEquals("C", c.converte(100));
+		
+	}
 }
